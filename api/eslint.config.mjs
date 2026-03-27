066198af8dac8af1e18dc69619ffeb207851df18
lab <lab@labs-Mac-mini.local>
2026-03-27 11:29:34 +0100

    Unused vars aan linter toegevoegd

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -3,6 +3,6 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, rules: { "no-unused-vars": ["error", { argsIgnorePattern: "^_" }] } },
   { files: ["__tests__/**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.jest } },
 ]);
