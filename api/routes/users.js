20b17360fbdfb5b0c9454bedf883aa5cf3ceae20
lab <lab@labs-Mac-mini.local>
2026-04-02 16:54:36 +0200

    API: publiceer user.created event op Redis bij POST /users

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,30 +2,22 @@ var express = require('express');
 
 var router = express.Router();
 
- 
-const { db } = require("../services/database");
-
- 
-/* GET users listing. */
+const { db } = require('../services/database');
+const { publisher } = require('../services/redis');
 
 router.get('/', async function(req, res) {
-
   let users = await db.collection('users').find().toArray();
-
   res.json(users);
-
 });
 
- 
-router.post('/', function(req, res){
-
+router.post('/', function(req, res) {
   db.collection('users').insertOne(req.body)
-
-    .then((user) => res.status(201).json({ "id": user.insertedId }))
-
+    .then((user) => {
+      publisher.publish('user.created', JSON.stringify({ id: user.insertedId, ...req.body }));
+      res.status(201).json({ id: user.insertedId });
+    })
     .catch(err => res.status(500).json(err));
-
-})
+});
 
 
 module.exports = router;
